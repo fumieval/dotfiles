c370b686085dad5a806a1bdc8020656de56a879a
Fumiaki Kinoshita <fumiexcel@gmail.com>
2017-02-21 13:19:49 +0900

    initial commit

---
+++ .bash_aliases
@@ -0,0 +1,9 @@
+alias ll='ls -alF'
+alias la='ls -A'
+alias l='ls -CF'
+
+alias gd='git diff'
+alias gf='git fetch'
+alias rebase='git rebase -i'
+
+
---
+++ setup.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+FILES=(.bashrc .bash_aliases .tmux.conf .vimrc)
+
+for FILE in "${FILES[@]}"; do
+  ln -s $DIR/$FILE ~/$FILE
+done
